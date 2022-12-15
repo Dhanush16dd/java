37c9f77162612cd4d04a5f7574ea0803c407d0ee
Dhanush16dd <dhanush16fk@gmail.com>
2022-12-15 12:07:08 +0530

    change 4

--- threading.java
+++ threading.java
@@ -6,29 +6,35 @@
         multiThread t2 = new multiThread();
         // Thread ty2 = new Thread(t2);
         t2.setName("Thread 2");
+        multiThread t3 = new multiThread();
+        // Thread ty2 = new Thread(t3);
+        t3.setName("Thread 3");
        
         t1.start();
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        // try {
+        //     t1.join();
+        // } catch (InterruptedException e) {
+        //     // TODO Auto-generated catch block
+        //     e.printStackTrace();
+        // }
         t2.start();
+        t3.start();
          
      }  
     }  
 class multiThread extends Thread{  
     public void run(){  
-        for (int i = 1; i <= 5; i++) {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            System.out.println(i+"Thread is running..."+Thread.currentThread().getName());  
-        }
+        // for (int i = 1; i <= 5; i++) {
+        //     // try {
+        //     //     Thread.sleep(2000);
+        //     // } catch (InterruptedException e) {
+        //     //     // TODO Auto-generated catch block
+        //     //     e.printStackTrace();
+        //     // }
+            System.out.println(Thread.currentThread().getName()+"thread started");  
+            System.out.println(Thread.currentThread().getName()+"thread ended");  
+            // Thread.yield();
+        // }
        
    
     }
